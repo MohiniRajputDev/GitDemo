52e5ad80771299c9849fd03bc9f51b8a007d4b88
MOHINI <MOHINI@DESKTOP-474K4SE>
2024-04-02 11:17:02 +0530

    by moh

--- GitDemo11/src/com/cjc/Demo.java
+++ GitDemo11/src/com/cjc/Demo.java
@@ -2,8 +2,14 @@ package com.cjc;
 
 public class Demo {
 	
+	
+	 public void m1() {
+		 System.out.println("m1");
+	 }
 	public static void main(String[] args) {
 		System.out.println("This is Git Demo");
 	}
+	
+	
 
 }
