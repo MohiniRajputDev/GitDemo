901287c46cfbdf9d8eed3747e0f868eed6dd1ba1
MohiniRajputDev <165640818+MohiniRajputDev@users.noreply.github.com>
2024-04-02 11:19:43 +0530

    Update Demo.java
    by moh

--- GitDemo11/src/com/cjc/Demo.java
+++ GitDemo11/src/com/cjc/Demo.java
@@ -10,6 +10,10 @@ public class Demo {
 	 public void m2() {
 		 System.out.println("m2");
 	 }
+
+	public void m3() {
+		 System.out.println("m3");
+	 }
 	 
 	public static void main(String[] args) {
 		System.out.println("This is Git Demo");
